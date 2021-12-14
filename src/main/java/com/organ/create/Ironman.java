eee28980d41c5ebba72eb3fd2820530ef4ed5214
RANA PRATHAP <RANA PRATHAP@LAPTOP-NR7AARSV>
2021-12-14 11:06:29 +0530

    commit

---
+++ src/main/java/com/organ/create/Ironman.java
@@ -0,0 +1,9 @@
+package com.organ.create;
+
+public class Ironman {
+
+	public static void main(String[] args) 
+	{
+	 System.out.println("Party ladha pushpa");
+	}
+}
---
+++ src/test/java/com/contact/create/MyRide.java
@@ -0,0 +1,10 @@
+package com.contact.create;
+
+public class MyRide {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello classmate");
+	}
+
+}
